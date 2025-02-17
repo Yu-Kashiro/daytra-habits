819c07cc9ac8d115947c740bc6ad60da8db6113a
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-02-17 17:00:42 +0900

    プロフィールモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_17_050228) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_17_055327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "nickname", null: false
+    t.integer "course", null: false
+    t.integer "area", null: false
+    t.integer "gender", null: false
+    t.date "birthday", null: false
+    t.text "introduction"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

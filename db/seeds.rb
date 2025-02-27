6c51790c7a7a8f51319cbaefc9eddf7de2e45b54
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-02-27 15:28:10 +0900

    I18nを適用

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,24 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-100.times do
-  user = User.create!(
-    email: Faker::Internet.email,
-    password: 'password'
-  )
+# 100.times do
+#   user = User.create!(
+#     email: Faker::Internet.email,
+#     password: 'password'
+#   )
 
-  profile = user.build_profile(
-    nickname: Faker::Name.name,
-    course: rand(1..15),
-    area: rand(1..47),
-    gender: rand(0..2),
-    birthday: Faker::Date.birthday(min_age: 6, max_age: 120),
-    introduction: Faker::Lorem.paragraph
-  )
-  profile.save!
+#   profile = user.build_profile(
+#     nickname: Faker::Name.name,
+#     course: rand(1..15),
+#     area: rand(1..47),
+#     gender: rand(0..2),
+#     birthday: Faker::Date.birthday(min_age: 6, max_age: 120),
+#     introduction: Faker::Lorem.paragraph
+#   )
+#   profile.save!
+# end
+
+(1..25).each do |day|
+  StudyRecord.create(user_id: 101, study_date: Date.new(2025, 2, day))
 end
-
 

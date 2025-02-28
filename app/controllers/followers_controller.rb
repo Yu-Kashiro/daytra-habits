ad6efb266c1950bbe91daae6556ca56a62e2fb35
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-02-28 16:19:30 +0900

    その他

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,4 +1,6 @@
 class FollowersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @account = User.find(params[:account_id])
     @followers = @account.followers
--- app/controllers/study_records_controller.rb
+++ app/controllers/study_records_controller.rb
@@ -3,9 +3,7 @@ class StudyRecordsController < ApplicationController
 
   def index
     start_date = Date.today
-    @study_records = current_user.study_records
-                                 .where(study_date: start_date.beginning_of_month..start_date.end_of_month)
-                                 .order(study_date: :desc)
+    @study_records = current_user.study_records.order(study_date: :desc)
     @period = number_of_running_days
     @total_days = current_user.study_records.count
   end
@@ -18,7 +16,7 @@ class StudyRecordsController < ApplicationController
       StudyRecord.create(study_date: today, user_id: current_user.id)
       flash[:notice] = '学習記録を追加しました！'
     end
-    redirect_to study_records_path
+    redirect_to root_path
   end
 
   private
